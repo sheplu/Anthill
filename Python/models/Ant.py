e3aff304f411ff3110986571639097f604b404f1
Sheplu <jean.burellier@gmail.com>
2017-01-01 15:02:40 +0100

    ant attributs

--- Python/models/Ant.py
+++ Python/models/Ant.py
@@ -3,6 +3,13 @@
 
 class Ant:
 	"""docstring for Ant"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self):
+		self.id = arg
+		self.generation = generation
+		self.age = 0
+		self.maxAge = maxAge
+		self.future =  future
+		self.religious = religious
+		self.healthy = healthy
+		self.life = 100
 		
